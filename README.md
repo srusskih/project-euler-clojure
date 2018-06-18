504152984e90ca8f17a74a3cd80fdcb335675b58
srusskih <smilemouse2002@ukr.net>
2018-06-18 11:56:24 +0300

    add project structure

---
+++ README.md
@@ -0,0 +1,11 @@
+# project-euler-clojure
+
+My Clojure education project.
+
+## Usage
+
+I hope you will never use it.
+
+## License
+
+Distributed under the MIT License
---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject project-euler-clojure "0.1.0-SNAPSHOT"
+  :description "My Clojure education project"
+  :license {:name "MIT"
+            :url "./LICENSE.txt"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
