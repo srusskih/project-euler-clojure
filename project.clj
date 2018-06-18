46436976436ad2c95e1c25788184606f11ce94d8
srusskih <smilemouse2002@ukr.net>
2018-06-18 15:13:59 +0300

    autodiscover problemsolvers

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "My Clojure education project"
   :license {:name "MIT"
             :url "./LICENSE.txt"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :main project-euler-clojure.core)
--- src/project_euler_clojure/core.clj
+++ src/project_euler_clojure/core.clj
@@ -1,6 +1,23 @@
-(ns project-euler-clojure.core)
+(ns project-euler-clojure.core
+  (:require [project-euler-clojure
+             helloworld
+             problem1]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn get-project-ns []
+    (filter #(clojure.string/starts-with?
+               (name (ns-name %))
+               "project-euler-clojure.")
+            (all-ns)))
+
+(defn resolve-solver [target-ns]
+  (ns-resolve target-ns (symbol "solve")))
+
+
+(defn get-problem-solvers [ns-with-problems]
+  (filter #(not (nil? %))
+          (map resolve-solver ns-with-problems)))
+
+
+(defn -main [& args]
+  (doseq [solver (get-problem-solvers (get-project-ns))] (solver))
+)
