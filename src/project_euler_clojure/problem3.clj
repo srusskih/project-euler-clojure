e25c8c3eb0036260f5b00e7802b912b92464a2b4
srusskih <smilemouse2002@ukr.net>
2018-06-20 12:56:55 +0300

    implement ferma method

--- src/project_euler_clojure/problem3.clj
+++ src/project_euler_clojure/problem3.clj
@@ -10,7 +10,6 @@
 
 (defn get-multipliers
   [n]
-  (if (1 n) [n, 1])
   (def s (get-s n))
   (loop [k 1]
     (def y (get-y s k n))
@@ -18,29 +17,42 @@
     (def a (get-a x y))
     (def b (get-b x y))
     (println k x y a b)
-    (if (no-rem y)
-      [a b]
-      (recur (inc k)))))
+    (cond 
+      (no-rem y) [a b]
+      (no-rem (Math/sqrt n)) [(Math/sqrt n) (Math/sqrt n)]
+      (even? (int n)) [2 (/ n 2)]
+      (some (partial <= n) [x y]) [n 1]
+      :else (recur (inc k)))))
+
+(defn ferma
+  [n]
+  (cond
+    (even? (int n)) [(/ n 2) 2]
+    :else
+      (loop [k 0]
+        (def a (+ (Math/ceil (Math/sqrt n)) k))
+        (def b2 (- (* a a) n))
+        (def b (Math/sqrt b2))
+        ; (println k ":" a b2 b)
+        (if (no-rem b)
+         [(+ b a) (- a b)]
+         (recur (inc k))))))
 
 (defn trie 
   [n] 
   (tree-seq
     ; branch?
     (fn [a]
-     ;(println ":branch?" a)
-     (and (not= 1 (int a))
-          (not= 1 (int (apply min (get-multipliers a)))))
-     )
+      ; (println ":branch?" a)
+      (not= 1 (int (apply min (ferma a)))))
     ; generate children
     (fn [a]
-     ;(println ":get-children" a)
-     (if (not= 1 (int a))
-      (get-multipliers a))
-     )
-     n))
+      ; (println ":get-children" a)
+      (ferma a))
+    n))
 
 (defn solve-fn [] (
-  (max (rest (trie 89755))
+  (max (rest (trie 89755))  ; 600851475143
 ))
 
 (defn solve
